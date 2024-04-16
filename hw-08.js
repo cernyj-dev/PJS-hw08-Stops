c9a878618d190df998be1b4c821c5cb11c1c72e9
Oldřich Malec <oldamalec@gmail.com>
2024-04-16 20:38:09 +0200

    HW 08 assignment

--- hw-08.js
+++ hw-08.js
@@ -1,5 +1,6 @@
 /**
  * This function will be called when DOM is ready and it's elements can be accessed by JS
+ * @see hw-08-index.html for detailed assignment
  */
 function init() {
     // TODO solution goes here
